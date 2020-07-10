0115069e98e5900ccd23ef9f447a26f367cb950a
smithasutar <55716185+smithasutar@users.noreply.github.com>
2020-07-10 19:49:41 -0400

    Adjusted play and quit buttons

--- src/Components/Buttons/Buttons.js
+++ src/Components/Buttons/Buttons.js
@@ -1,23 +1,40 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Buttons.css";
 
-export default function Buttons ({openChest, closeChest, showGame}) {
+export default function Buttons({ openChest, closeChest, showGame }) {
+  const [Play, setPlay] = useState(false);
 
- const [Play, setPlay] = useState(false);
+  // determines whether to hide or show Play button
+  const PlayButton = () => {
+    setPlay(!Play);
+  };
 
- // determines whether to hide or show Play button
-   const PlayButton = () => {
-     setPlay(!Play);
-   }
-
-   // display Play button or Play Again button
+  // display Play button or Play Again button
   return (
-    <div> 
-    
-    <button id="flexx" className={Play ? "hide" : "btn btn-success"} onClick={() => {openChest();PlayButton();}}>Play</button>
-    <button id="flexx" className={Play ? "btn btn-success" : "hide"} onClick={() => {closeChest();PlayButton();}}>Play Again</button>
-    <button id="quit" className="btn btn-warning" onClick={showGame}>Quit</button>
-    
+    <div>
+      <button
+        id="flexx"
+        className={Play ? "hide" : "btn btn-success"}
+        onClick={() => {
+          openChest();
+          PlayButton();
+        }}
+      >
+        Play
+      </button>
+      <button
+        id="flexx"
+        className={Play ? "btn btn-success" : "hide"}
+        onClick={() => {
+          closeChest();
+          PlayButton();
+        }}
+      >
+        Play Again
+      </button>
+      <button id="quit" className="btn btn-warning" onClick={showGame}>
+        Quit
+      </button>
     </div>
-    );
-  }
+  );
+}
