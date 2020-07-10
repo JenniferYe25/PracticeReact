0defe98647bb2a648a50ed900a5616b9eb5164b5
Jothine Nandakumar <60521482+Jothine123@users.noreply.github.com>
2020-07-10 11:15:03 -0400

    Changed sizes of images
    Made changes to Chest.js, added height and width to image tags

--- src/Components/Chest/Chest.js
+++ src/Components/Chest/Chest.js
@@ -5,12 +5,19 @@ export default function Chest(props) {
 
   //if chest opened is false, it displays the closed chest image
   if (props.opened === false) {
-    image = <img src="/Chests.png" alt="chest" />;
+    image = <img src="/Chests.png" alt="chest" width="200px" height="200px" />;
   }
 
   //if chest opened is true, it displays the prize based on the index
   else {
-    image = <img src={"/Prize" + (props.chestNumber + 1) + ".png"} alt="chest" />;
+    image = (
+      <img
+        src={"/Prize" + (props.chestNumber + 1) + ".png"}
+        alt="chest"
+        width="200px"
+        height="200px"
+      />
+    );
   }
 
   //returns the image based on the conditions above
