704cfaea546e07407298efdd26d1cf509ae0cd60
Jennifer Ye <jennifer.ye3@gmail.com>
2020-07-14 12:30:52 -0400

    added onclick sound on start button

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { RemoveScrollBar } from "react-remove-scroll-bar";
 import "./Homepage.css";
+import useSound from 'use-sound';
+import sound from '../Homepage/pop.mp3';
 
 export default function Homepage({ showGame }) {
   // play button (change showGame state)
+  const [play] = useSound(sound);
+  function startClicked () {
+    showGame();
+    play();
+  }
   return (
     <div id="flex">
       <RemoveScrollBar />
@@ -12,10 +19,10 @@ export default function Homepage({ showGame }) {
           <span>Treasure Chest Randomizer</span>
         </h1>
       </div>
-      <button id="start" className="btn btn-lg btn-danger" onClick={showGame}>
+      <button id="start" className="btn btn-lg btn-danger" onClick={startClicked}>
         Start
       </button>
-      <img id="background" src="/background.jpg" alt="backgroundImg" />
+      {/* <img id="background" src="/background.jpg" alt="backgroundImg" /> */}
     </div>
   );
 }
